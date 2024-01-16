5a9de9c2edca6476bea0a72bba0c03cea20c6b76
simran332 <skaur332@flemingcollege.ca>
2024-01-16 11:52:44 -0500

    Modified both script files in the repo clone
    - Deleted a print() statement  from script1.py
    - Added two print() statements  to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Beautiful is better than ugly.")
 print("Errors should never pass silently.")
+print("If the implementation is easy to explain, it may be a good idea.")
